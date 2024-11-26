b9bde21168a4dce8854f294949681241845287ed
Ufuk ABRAVACI <ufkk1262@gmail.com>
2024-11-27 00:49:02 +0300

    MenuItem CRUD Operations Implemented.

--- AbraFood_API/AbraFood_API/Controllers/MenuItemController.cs
+++ AbraFood_API/AbraFood_API/Controllers/MenuItemController.cs
@@ -1,8 +1,13 @@
 ﻿using AbraFood_API.Data;
 using AbraFood_API.Models;
+using AbraFood_API.Models.Dto;
+using AbraFood_API.Services;
+using AbraFood_API.Utility;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AbraFood_API.Controllers
 {
@@ -11,10 +16,12 @@ namespace AbraFood_API.Controllers
     public class MenuItemController : ControllerBase
     {
         private readonly ApplicationDbContext _db;
+        private readonly IBlobService _blobService;
         private ApiResponse _response;
-        public MenuItemController(ApplicationDbContext db)
+        public MenuItemController(ApplicationDbContext db, IBlobService blobService)
         {
             _db = db;
+            _blobService = blobService;
             _response = new ApiResponse();
         }
 
@@ -25,21 +32,160 @@ namespace AbraFood_API.Controllers
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
         }
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}",Name = "GetMenuItem")]
         public async Task<IActionResult> GetMenuItem(int id)
         {
             if (id == 0) { 
-            _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
                 return BadRequest(_response);
             }
             MenuItem menuItem = _db.MenuItems.FirstOrDefault(x => x.Id == id);
             if (menuItem == null) {
                 _response.StatusCode= HttpStatusCode.NotFound;
+                _response.IsSuccess = false;
                 return NotFound(_response);
             }
             _response.Result = menuItem;
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateMenuItem([FromForm] MenuItemCreateDTO menuItemCreateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (menuItemCreateDTO.File == null || menuItemCreateDTO.File.Length == 0)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        return BadRequest();
+                    }
+                    string fileName = $"{Guid.NewGuid()}{Path.GetExtension(menuItemCreateDTO.File.FileName)}";
+                    MenuItem menuItemToCreate = new()
+                    {
+                        Name = menuItemCreateDTO.Name,
+                        Price = menuItemCreateDTO.Price,
+                        Category = menuItemCreateDTO.Category,
+                        SpecialTag = menuItemCreateDTO.SpecialTag,
+                        Description = menuItemCreateDTO.Description,
+                        //Upload the blob and it will return back the URL which we will save in the image.
+                        Image = await _blobService.UploadBlob(fileName, SD.SD_Storage_Container, menuItemCreateDTO.File)
+                    };
+                    _db.MenuItems.Add(menuItemToCreate);
+                    _db.SaveChanges();
+                    _response.Result = menuItemToCreate;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetMenuItem",new {id=menuItemToCreate.Id},_response);
+                    
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> UpdateMenuItem(int id,[FromForm] MenuItemUpdateDTO menuItemUpdateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (menuItemUpdateDTO == null || id != menuItemUpdateDTO.Id)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        return BadRequest();
+                    }
+
+                    MenuItem menuItemFromDb = await _db.MenuItems.FindAsync(id);
+                    if(menuItemFromDb == null)
+                    {
+                        return BadRequest();
+                    }
+
+                    menuItemFromDb.Name = menuItemUpdateDTO.Name;
+                    menuItemFromDb.Price = menuItemUpdateDTO.Price;
+                    menuItemFromDb.Category = menuItemUpdateDTO.Category;
+                    menuItemFromDb.SpecialTag = menuItemUpdateDTO.SpecialTag;
+                    menuItemFromDb.Description = menuItemUpdateDTO.Description;
+
+                    if (menuItemUpdateDTO.File != null && menuItemUpdateDTO.File.Length > 0) 
+                    {
+                        string fileName = $"{Guid.NewGuid()}{Path.GetExtension(menuItemUpdateDTO.File.FileName)}";
+                        await _blobService.DeleteBlob(menuItemFromDb.Image.Split("/").Last(), SD.SD_Storage_Container);
+                        menuItemFromDb.Image = await _blobService.UploadBlob(fileName, SD.SD_Storage_Container, menuItemUpdateDTO.File);
+                    }
+
+                    
+                    _db.MenuItems.Update(menuItemFromDb);
+                    _db.SaveChanges();
+                    
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> DeleteMenuItem(int id)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if ( id == 0)
+                    {
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.IsSuccess = false;
+                        return BadRequest();
+                    }
+
+                    MenuItem menuItemFromDb = await _db.MenuItems.FindAsync(id);
+                    if (menuItemFromDb == null)
+                    {
+                        return BadRequest();
+                    }
+                    await _blobService.DeleteBlob(menuItemFromDb.Image.Split("/").Last(), SD.SD_Storage_Container);
+                    int miliseconds = 1500;
+                    Thread.Sleep(miliseconds);
+                    _db.MenuItems.Remove(menuItemFromDb);
+                    _db.SaveChanges();
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
         }
     }
 }
---
+++ AbraFood_API/AbraFood_API/Utility/SD.cs
@@ -0,0 +1,7 @@
+﻿namespace AbraFood_API.Utility
+{
+    public static class SD
+    {
+        public const string SD_Storage_Container = "abrafood";
+    }
+}
