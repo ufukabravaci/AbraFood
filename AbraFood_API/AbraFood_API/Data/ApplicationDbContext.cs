c4e28fcdbf43e21cf3bf25ee7ccd1303079df243
Ufuk ABRAVACI <ufkk1262@gmail.com>
2024-11-30 16:01:44 +0300

    ShoppingCart and CartItem models created and migrated to Db

--- AbraFood_API/AbraFood_API/Data/ApplicationDbContext.cs
+++ AbraFood_API/AbraFood_API/Data/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ namespace AbraFood_API.Data
             
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ AbraFood_API/AbraFood_API/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AbraFood_API.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        
+
+        public ICollection<CartItem> CartItems { get; set; }
+
+        [NotMapped] //Doesn't create a column in database.
+        public double CartTotal { get; set; }
+        [NotMapped]
+        public string StripePaymentIntentId { get; set; }
+        [NotMapped]
+        public string ClientSecret { get; set; }
+    }
+    
+
+}
